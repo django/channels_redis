16bdf4d3951c7f88b96bd922b5d4273cd93c4d98
Andrew Godwin <andrew@aeracode.org>
2016-03-27 11:11:14 -0700

    Update to match new asgiref test style

--- test_asgi_redis.py
+++ test_asgi_redis.py
@@ -1,5 +1,10 @@
+import unittest
 from asgi_redis import RedisChannelLayer
-from asgiref.conformance import make_tests
+from asgiref.conformance import ConformanceTestCase
 
-channel_layer = RedisChannelLayer(expiry=1)
-RedisTests = make_tests(channel_layer, expiry_delay=1.1)
+
+# Default conformance tests
+class RedisLayerTests(ConformanceTestCase):
+
+    channel_layer = RedisChannelLayer(expiry=1, group_expiry=2)
+    expiry_delay = 1.1
