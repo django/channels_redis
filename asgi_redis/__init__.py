515de7bc15b2e7e0c2874b31390d93894b7c38fd
Luke Hodkinson <furious.luke@gmail.com>
2016-09-01 13:05:54 +1000

    * Modified RedisChannelLayer to allow a different mechanism   for generating Redis connections. * Made a new layer, DjangoRedisChannelLayer, for getting connections   using `django-redis`'s connection pool. This way I can configure   the connection pool to limit the number of connections, and use   a blocking connection, and have Channels observe the configuration.

--- asgi_redis/__init__.py
+++ asgi_redis/__init__.py
@@ -1,5 +1,5 @@
 import pkg_resources
-from .core import RedisChannelLayer
+from .core import RedisChannelLayer, DjangoRedisChannelLayer
 from .local import RedisLocalChannelLayer
 
 __version__ = pkg_resources.require('asgi_redis')[0].version
