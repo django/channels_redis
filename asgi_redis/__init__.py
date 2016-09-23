20f7102daf411a07ec922fceb2fac6c00356a84b
Krukov Dima <glebov.ru@gmail.com>
2016-09-23 20:44:02 +0300

    Revert "Version in function"
    This reverts commit b3ecaffc2f30060f8143c92e9b6dec30ef35d51a.

--- asgi_redis/__init__.py
+++ asgi_redis/__init__.py
@@ -2,6 +2,4 @@ import pkg_resources
 from .core import RedisChannelLayer
 from .local import RedisLocalChannelLayer
 
-
-def get_version():
-    return pkg_resources.require('asgi_redis')[0].version
+__version__ = pkg_resources.require('asgi_redis')[0].version
