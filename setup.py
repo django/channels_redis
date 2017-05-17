63ddf2aea55cc9a5578aa29d256b20f39227ff1a
Ben Greenberg <bgreenberg@eventbrite.com>
2017-05-17 10:46:29 -0700

    remove ipdb from test requirements. You can install it if you really need it

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ readme_path = os.path.join(os.path.dirname(__file__), "README.rst")
 
 crypto_requires = ['cryptography>=1.3.0']
 twisted_requires = ['twisted>=17.1', 'txredisapi']
-tests_require = ['tox', 'asgi_ipc', 'ipdb']
+tests_require = ['tox', 'asgi_ipc']
 
 
 setup(
