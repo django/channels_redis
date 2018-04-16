9bc120084a2d770e5c3859c652afd7db030a833f
mmcardle <mmcardle@users.noreply.github.com>
2018-04-16 18:22:09 +0100

    Added channel capacity check to lua-based group_send (#101)
    Fixed #96.

--- channels_redis/core.py
+++ channels_redis/core.py
@@ -313,7 +313,7 @@ class RedisChannelLayer(BaseChannelLayer):
             # Return current lot
             channel_names = [x.decode("utf8") for x in await connection.zrange(key, 0, -1)]
 
-        connection_to_channels, channel_to_message, channel_to_key = \
+        connection_to_channels, channel_to_message, channel_to_capacity, channel_to_key = \
             self._map_channel_to_connection(channel_names, message)
 
         for connection_index, channel_redis_keys in connection_to_channels.items():
@@ -323,13 +323,19 @@ class RedisChannelLayer(BaseChannelLayer):
             # __asgi_channel__ key.
             group_send_lua = """
                 for i=1,#KEYS do
-                    redis.call('RPUSH', KEYS[i], ARGV[i])
-                    redis.call('EXPIRE', KEYS[i], %d)
+                    if redis.call('LLEN', KEYS[i]) < tonumber(ARGV[i + #KEYS]) then
+                        redis.call('RPUSH', KEYS[i], ARGV[i])
+                        redis.call('EXPIRE', KEYS[i], %d)
+                    end
                 end
             """ % self.expiry
 
             # We need to filter the messages to keep those related to the connection
             args = [channel_to_message[channel_name] for channel_name in channel_names
+                    if channel_to_key[channel_name] in channel_redis_keys]
+
+            # We need to send the capacity for each channel
+            args += [channel_to_capacity[channel_name] for channel_name in channel_names
                     if channel_to_key[channel_name] in channel_redis_keys]
 
             async with self.connection(connection_index) as connection:
@@ -342,10 +348,11 @@ class RedisChannelLayer(BaseChannelLayer):
         Also for each channel create a message specific to that channel, adding
         the __asgi_channel__ key to the message
         We also return a mapping from channel names to their corresponding Redis
-        keys
+        keys, and a mapping of channels to their capacity
         """
         connection_to_channels = collections.defaultdict(list)
         channel_to_message = dict()
+        channel_to_capacity = dict()
         channel_to_key = dict()
 
         for channel in channel_names:
@@ -357,11 +364,12 @@ class RedisChannelLayer(BaseChannelLayer):
             channel_key = self.prefix + channel_non_local_name
             idx = self.consistent_hash(channel_non_local_name)
             connection_to_channels[idx].append(channel_key)
+            channel_to_capacity[channel] = self.get_capacity(message)
             channel_to_message[channel] = self.serialize(message)
             # We build a
             channel_to_key[channel] = channel_key
 
-        return connection_to_channels, channel_to_message, channel_to_key
+        return connection_to_channels, channel_to_message, channel_to_capacity, channel_to_key
 
     def _group_key(self, group):
         """
