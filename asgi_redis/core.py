a2dedaa73800a907b08d8fc601fb5e756aa878d0
P. Christeas <xrg@pefnos.com>
2019-08-02 00:19:27 +0300

    core: support for python-redis 3.x

--- asgi_redis/core.py
+++ asgi_redis/core.py
@@ -392,7 +392,11 @@ class BaseRedisChannelLayer(BaseChannelLayer):
             statistics[self.STAT_MESSAGES_PENDING] += connection.llen(channel_key)
             oldest_message = connection.lindex(channel_key, 0)
             if oldest_message:
-                messages_age = self.expiry - (connection.ttl(oldest_message) or 0)
+                t = connection.ttl(oldest_message)
+                if t and t > 0:
+                    messages_age = self.expiry - t
+                else:
+                    messages_age = 0
                 statistics[self.STAT_MESSAGES_MAX_AGE] = max(statistics[self.STAT_MESSAGES_MAX_AGE], messages_age)
         return statistics
 
