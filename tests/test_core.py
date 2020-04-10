7083361673127199cf5964b9bb6c0923dbbeae65
Felix Pan <tinylambda@gmail.com>
2020-04-10 23:24:06 +0800

    code format

--- tests/test_core.py
+++ tests/test_core.py
@@ -387,8 +387,7 @@ async def test_connection_pool_pop():
     conn_not_exists = None
     # Will not return a bad connection to the pool
     connection_pool.push(conn_not_exists)
-    all_connections_in_this_pool = [conn for conns in connection_pool.conn_map.values()
-                                    for conn in conns]
+    all_connections_in_this_pool = [conn for conns in connection_pool.conn_map.values() for conn in conns]
     assert conn_not_exists not in all_connections_in_this_pool
 
     # Ensure the closed connection is inside the pool
