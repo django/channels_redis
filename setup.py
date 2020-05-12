42f6bce7bd9efeaf68868763625a5e91674e7b8c
tarikki <tariq.alsadoon@gmail.com>
2020-05-12 20:53:27 +0300

    Bump pytest to 5.4.0 to resolve version conflicts

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ readme = open(join(dirname(__file__), "README.rst")).read()
 crypto_requires = ["cryptography>=1.3.0"]
 
 test_requires = crypto_requires + [
-    "pytest~=3.6.0",
+    "pytest~=5.4.0",
     "pytest-asyncio~=0.8",
     "async_generator~=1.8",
     "async-timeout~=2.0",
